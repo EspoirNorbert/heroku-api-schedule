cddb6337fab3c563af414a3ec426b75b6a9a235c
EspoirNorbert <espoirnorbert@gmail.com>
2021-07-01 20:33:10 +0200

    reglage de la ligne 7 avec l'ajout du nouveau plugin postgresql

--- config.py
+++ config.py
@@ -4,6 +4,6 @@ import os
 if os.environ.get('DATABASE_URL') is None:
     SQLALCHEMY_DATABASE_URI = "postgresql://postgres:root@localhost:5432/schedule_db"
 else:
-    SQLALCHEMY_DATABASE_URI = os.environ['DATABASE_URL']
+    SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL').replace("://", "ql://", 1)
 
 SQLALCHEMY_TRACK_MODIFICATIONS = False
